6f2f99b9bbe13dd6d25d6cc3a77c4a43df7d78d3
liuguangming <jsliugm@gmail.com>
2020-06-26 19:50:57 +0800

    tt

--- src/main/java/com/universe/demo/springboot/controller/HelloController.java
+++ src/main/java/com/universe/demo/springboot/controller/HelloController.java
@@ -1,19 +1,37 @@
 package com.universe.demo.springboot.controller;
 
+import com.universe.demo.springboot.dto.PersonDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by jsliu on 2019/1/13.
  */
 @RestController
+@Slf4j
 public class HelloController {
     @RequestMapping("/hello")
-    public Object hello(){
+    public Object hello() {
         return "hello world";
     }
+
     @RequestMapping("/hello2")
-    public Object hello2(){
+    public Object hello2() {
         return "hello2";
+    }
+
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    public Object hello3(@RequestBody String abc) {
+        log.info(abc);
+        return "hello3";
+    }
+
+    @RequestMapping(value = "/hello4", method = RequestMethod.POST)
+    public Object hello4(@RequestBody PersonDto personDto) {
+        log.info(personDto.getName());
+        return "hello4";
     }
 }
---
+++ src/main/java/com/universe/demo/springboot/dto/PersonDto.java
@@ -0,0 +1,6 @@
+package com.universe.demo.springboot.dto;
+
+import com.universe.demo.springboot.entity.Person;
+
+public class PersonDto extends Person {
+}
